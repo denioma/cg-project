751de62e75753ea62e672201b1491a980ed9c699
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-11-11 00:08:59 +0000

    Comments

--- ProjetoFinal/src/main.cpp
+++ ProjetoFinal/src/main.cpp
@@ -8,11 +8,6 @@
 #include "libs/geometry.h"
 #include "libs/objects.h"
 
-constexpr auto windowTitle = "Simple Audio Mixer";
-constexpr auto world = 10;
-
-int windowWidth = 800, windowHeight = 600;
-
 void init();
 void draw();
 void keyboard(unsigned char, int, int);
@@ -23,6 +18,12 @@ void mouse(int, int);
 void wheel(int, int, int, int);
 void reshape(int, int);
 
+constexpr auto windowTitle = "Simple Audio Mixer";
+constexpr auto world = 10;
+
+// Window size (automatically refreshed by reshaping the window)
+int windowWidth = 800, windowHeight = 600;
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
@@ -32,33 +33,38 @@ int main(int argc, char** argv) {
 
 	init();
 
-	glutDisplayFunc(draw);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(special);
-	glutMotionFunc(mouse);
-	glutMouseFunc(wheel);
-	glutTimerFunc(0, timer, 0);
-	glutTimerFunc(0, eqTimer, 1);
-	glutReshapeFunc(reshape);
+	glutDisplayFunc(draw);			// Display Callback
+	glutKeyboardFunc(keyboard);		// Keyboard (ASCII) Callback
+	glutSpecialFunc(special);		// Keyboard (Non-ASCII) Callback
+	glutMotionFunc(mouse);			// Mouse Callback #1
+	glutMouseFunc(wheel);			// Mouse Callback #2
+	glutTimerFunc(0, timer, 0);		// Timer #1 - Redisplay
+	glutTimerFunc(0, eqTimer, 1);	// Timer #2 - EQ
+	glutReshapeFunc(reshape);		// Reshape Callback
 
 	glutMainLoop();
 	return 1;
 }
 
+//Draws the 3-dimensional axis
 void drawAxis() {
 	glLineWidth(1);
+
+	// X axis
 	glColor4d(RED);
 	glBegin(GL_LINES); {
 		glVertex3d(0, 0, 0);
 		glVertex3i(world, 0, 0);
 	} glEnd();
 
+	//Y axis
 	glColor4d(GREEN);
 	glBegin(GL_LINES); {
 		glVertex3d(0, 0, 0);
 		glVertex3d(0, world, 0);
 	} glEnd();
 
+	// Z axis
 	glColor4d(BLUE);
 	glBegin(GL_LINES); {
 		glVertex3d(0, 0, 0);
@@ -66,16 +72,17 @@ void drawAxis() {
 	} glEnd();
 }
 
-// Camera variables
+// Camera variables (using polar coordinates)
+constexpr auto angleMin = -20, angleMax = -340, angleIncrement = -5;
 const GLdouble visionRadius = 10, visionIncrement = 0.2, camIncrement = 0.25;
 GLdouble fov = 45, visionAngle = 0, vertAngle = M_PI/3;
-GLdouble obs[3] = {
-	
-};
+GLdouble obs[3];
+
+// Interactive elements
 mixerSettings interactive;
 bars eq;
-constexpr auto angleMin = -20, angleMax = -340, angleIncrement = -5;
 
+// OpenGL and interactive elements init
 void init() {
 	glClearColor(WHITE);
 	glEnable(GL_DEPTH_TEST);
@@ -95,14 +102,17 @@ void init() {
 	interactive.pressed4 = false;
 }
 
+// Object drawing calls
 void drawCalls(const GLboolean axis) {
 	if (axis) drawAxis();
 	mixer(&interactive, &eq);
 }
 
+// Draw calls
 void draw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	// Main view
 	glViewport(0, 0, windowWidth, windowHeight);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -116,6 +126,7 @@ void draw() {
 
 	drawCalls(true);
 
+	// Top-down view
 	glViewport(windowWidth - 100, windowHeight - 100, 100, 100);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -126,6 +137,7 @@ void draw() {
 
 	drawCalls(false);
 
+	// Front view
 	glViewport(windowWidth - 100, windowHeight - 200, 100, 100);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -139,6 +151,7 @@ void draw() {
 	glutSwapBuffers();
 }
 
+// Keyboard (ASCII) event handler
 void keyboard(unsigned char key, int x, int y) {
 	switch (tolower(key)) {
 	// Knob #1
@@ -186,25 +199,26 @@ void keyboard(unsigned char key, int x, int y) {
 		interactive.slider -= 0.1;
 		if (interactive.slider < -0.5) interactive.slider = -0.5;
 		break;
-	case 'z':
+	case 'z':	// Button #1
 		interactive.pressed1 = !interactive.pressed1;
 		break;
-	case 'x':
+	case 'x':	// Button #2
 		interactive.pressed2 = !interactive.pressed2;
 		break;
-	case 'c':
+	case 'c':	// Button #3
 		interactive.pressed3 = !interactive.pressed3;
 		break;
-	case 'v':
+	case 'v':	// Button #4
 		interactive.pressed4 = !interactive.pressed4;
 		break;
-		// Camera FOV controls
+	// Camera FOV controls
 	case '+':
 		if (fov - 3 > 0) fov -= 3;
 		break;
 	case '-':
 		fov += 3;
 		break;
+	// Quit
 	case 27:
 		glutLeaveMainLoop();
 		exit(0);
@@ -212,20 +226,19 @@ void keyboard(unsigned char key, int x, int y) {
 	}
 }
 
+
+// Keyboard (non-ascii) event handler
 void special(int key, int x, int y) {
 	switch (key) {
+	// Camera rotation (using polar coordinates)
 	case GLUT_KEY_UP:
-		//obs[1] -= camIncrement;
 		vertAngle -= visionIncrement;
 		if (vertAngle <= 0) vertAngle = 0.001;
 		break;
-	// Camera Y position
 	case GLUT_KEY_DOWN:
-		//obs[1] += camIncrement;
 		vertAngle += visionIncrement;
 		if (vertAngle > M_PI) vertAngle = M_PI;
 		break;
-	// Camera rotation (y plane)
 	case GLUT_KEY_LEFT:
 		visionAngle += visionIncrement;
 		break;
@@ -239,11 +252,13 @@ void special(int key, int x, int y) {
 	}
 }
 
+// C++ random generators
 std::random_device rd;
 std::uniform_real_distribution<GLdouble> d(0, 5);
 
 constexpr auto eqFps = 20, eqMsec = 1000 / eqFps;
 
+// Create random values for EQ bar scale
 void eqTimer(int value) {
 	glutTimerFunc(eqMsec, eqTimer, 1);
 	GLdouble slider = -interactive.slider + 0.5;
@@ -259,14 +274,17 @@ void eqTimer(int value) {
 
 constexpr auto fps = 60, msec = 1000 / fps;
 
+// Calls glutPostRedisplay at a rate of 60 fps, and handles button animation
 void timer(int value) {
 	glutTimerFunc(msec, timer, 0);
 
+	// Smooth press down
 	if (interactive.pressed1 && interactive.button1 >= -0.05) interactive.button1 -= 0.01;
 	if (interactive.pressed2 && interactive.button2 >= -0.05) interactive.button2 -= 0.01;
 	if (interactive.pressed3 && interactive.button3 >= -0.05) interactive.button3 -= 0.01;
 	if (interactive.pressed4 && interactive.button4 >= -0.05) interactive.button4 -= 0.01;
 
+	// Smooth bounce up
 	if (!interactive.pressed1 && interactive.button1 <= 0) interactive.button1 += 0.01;
 	if (!interactive.pressed2 && interactive.button2 <= 0) interactive.button2 += 0.01;
 	if (!interactive.pressed3 && interactive.button3 <= 0) interactive.button3 += 0.01;
@@ -276,6 +294,9 @@ void timer(int value) {
 }
 
 int prevX = 0, prevY = 0;
+
+// Handles orbital controls using the mouse
+// Drag the mouse to rotate around the object
 void mouse(int x, int y) {
 	if (x - prevX > 0) visionAngle -= visionIncrement/4;
 	if (x - prevX < 0) visionAngle += visionIncrement/4;
@@ -289,11 +310,13 @@ void mouse(int x, int y) {
 	prevY = y;
 }
 
+// Mouse wheel handler to control FOV (zoom)
 void wheel(int button, int state, int x, int y) {
 	if (button == 3 && state == GLUT_DOWN && fov - 3 > 0) fov -= 3;
 	if (button == 4 && state == GLUT_DOWN) fov += 3;
 }
 
+// Reshape handler to ensure resizing of viewport
 void reshape(int w, int h) {
 	if (h == 0) h = 1;
 
